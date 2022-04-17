66919b18294eb5302e34b22f1c68d15ff93afb5e
Caroline Huang <lh494@zhang-x1.ece.cornell.edu>
2022-04-17 13:36:08 -0400

    qfully_connected fixed

--- Inference_pytorch/models/simBNN.py
+++ Inference_pytorch/models/simBNN.py
@@ -39,10 +39,18 @@ class BinaryConv2d(nn.Conv2d):
         return F.conv2d(self.binarize(input), self.binarize(self.weight),
                         self.bias, self.stride, self.padding,
                         self.dilation, self.groups)
-    # 
+
+class BinaryFully_connected(nn.linear):
+    def __init__(self, in_features, out_features, bias = True, device = None, dtype = None):
+        super(BinaryFully_connected, self).__init__(in_features, out_features, bias, device, dtype)
+        
+        self.binarize = FastSign()
+
+    def forward(self, input):
+        return F.linear(self.binarize(input), self.binarize(self.weight), self.bias, self.device, self.dtype)
 
 class simBNN(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes):
         super(simBNN, self).__init__()
 
         # Stage 1 
@@ -63,7 +71,7 @@ class simBNN(nn.Module):
         self.bn3 = nn.BatchNorm2d(384, eps = eps, affine = affine, momentum = bn_mom)
 
         self.act_q4 = FastSign()
-        self.conv4 = nn.BinaryConv2d(in_channels = 384, out_channels = 384 ,kernel_size = (3, 3), padding = (1,1))
+        self.conv4 = BinaryConv2d(in_channels = 384, out_channels = 384 ,kernel_size = (3, 3), padding = (1,1))
         self.bn4 = nn.BatchNorm2d(384, eps = eps, affine = affine, momentum = bn_mom)
 
         self.act_q5 = FastSign()
@@ -73,16 +81,16 @@ class simBNN(nn.Module):
         # Stage 4
         self.flatten = nn.Flatten() # I am not really sure about how this flatten works 
         self.act_fc1 = FastSign()
-        self.fc1 = nn.linear(in_features, out_features ) # QFully_connected (num_hidden = 4096 ?)
-        self.bn6 = nn.BatchNorm2d(256, eps = eps, affine = affine, momentum = bn_mom)
+        self.fc1 = BinaryFully_connected(in_features =256, out_features=4096 )
+        self.bn6 = nn.BatchNorm2d(4096, eps = eps, affine = affine, momentum = bn_mom)
 
         # Stage 5
         self.act_fc2 = FastSign()
-        self.fc2 = nn.linear(in_features, out_features ) # QFully_connected (num_hidden = 4096 ?)
-        self.bn7 = nn.BatchNorm2d(256, eps = eps, affine = affine, momentum = bn_mom)
+        self.fc2 = BinaryFully_connected(in_features = 4096, out_features=4096 ) 
+        self.bn7 = nn.BatchNorm2d(4096, eps = eps, affine = affine, momentum = bn_mom)
 
         # Stage 6 
-        self.fc3 = nn.linear(in_features, out_features ) # Normal Fully_connected (num_hidden = num_classes)
+        self.fc3 = nn.linear(in_features = 4096, out_features =num_classes )
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self,x):
@@ -92,32 +100,32 @@ class simBNN(nn.Module):
         x=F.max_pool2d(x,kernel_size = (3, 3),stride = (2, 2))
 
         # Stage 2 
-        x=F.relu(self.act_q2(x)) 
+        x=self.act_q2(F.relu(x)) 
         x=self.conv2(x) 
         x=self.bn2(x) 
         x=F.max_pool2d(x,kernel_size = (3, 3),stride = (2, 2))
 
         #Stage 3 
-        x=F.relu(self.act_q3(x)) 
+        x=self.act_q3(F.relu(x)) 
         x=self.conv3(x) 
         x=self.bn3(x) 
-        x=F.relu(self.act_q4(x)) 
+        x=self.act_q4(F.relu(x)) 
         x=self.conv4(x) 
         x=self.bn4(x) 
-        x=F.relu(self.act_q5(x)) 
+        x=self.act_q5(F.relu(x)) 
         x=self.conv5(x) 
         x=self.bn5(x) 
         x=F.max_pool2d(x,kernel_size = (3, 3),stride = (2, 2))
 
         #Stage 4 
         x = self.flatten(x)
-        x=F.relu(self.act_fc1(x)) 
-        x = self.fc1(x) # QFully_connected (fast sign?) 
+        x=self.act_fc1(F.relu(x)) 
+        x = self.fc1(x)
         x=F.relu(self.bn6(x))
 
         #Stage 5 
-        x=F.relu(self.act_fc2(x)) 
-        x = self.fc2(x) # QFully_connected (fast sign?) 
+        x=self.act_fc2(F.relu(x)) 
+        x = self.fc2(x) 
         x=F.relu(self.bn7(x))
 
         #Stage 6 
